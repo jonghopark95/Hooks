7087daf3bf22295986924b3db74c40f492109d65
jonghopark95 <https://github.com/jonghopark95>
2020-08-06 03:28:30 +0900

    update usetabs

--- useState/useTabs.js
+++ useState/useTabs.js
@@ -1,10 +1,10 @@
 // API 등으로 받아온 JSON 파일을 버튼 등으로 컨트롤 할 수 있는 Hook
 
 export const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
   if (!allTabs || !Array.isArray(allTabs)) {
     return;
   }
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
   return {
     currentItem: allTabs[currentIndex],
     changeItem: setCurrentIndex,
